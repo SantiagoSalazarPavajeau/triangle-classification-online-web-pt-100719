ac9e5b0051ef99e1403cb1e52e59b46d7cec35cd
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-11-10 17:18:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,21 +10,19 @@ class Triangle
   
   def kind
     @a, @b, @c = [@a, @b, @c].sort
-    raise TriangleError if @a <= 0 or @a + @b <= @c
-    return :equilateral if @a == @c
-    return :isosceles if @a == @b or @b == @c
-    return :scalene
-    # if @a == @b && @a == @c && @b == @c
-    #   :equilateral
-    # elsif @b==@c || @a==@b || @c==@a 
-    #   :isosceles
-    # elsif @a <= 0 || @b <= 0 || @c <= 0
-    #   raise TriangleError
-    # elsif @a >= @b+@c || @c >= @b+@a || @b >= @a+@c
-    #   raise TriangleError
-    # else 
-    #   :scalene
-    # end
+    # raise TriangleError if @a <= 0 or @a + @b <= @c
+    # return :equilateral if @a == @c
+    # return :isosceles if @a == @b or @b == @c
+    # return :scalene
+    if @a == @c
+      :equilateral
+    elsif @a == @b || @b == @c 
+      :isosceles
+    elsif @a <= 0 || @a + @b <= @c
+      raise TriangleError
+    else 
+      :scalene
+    end
   end
   
   class TriangleError < StandardError
